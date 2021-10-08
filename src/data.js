2e6fe0922d4fa555bd86c02859f38a565682dc54
Tyler Lawson <tylaw93@yahoo.com>
2021-10-08 02:06:40 -0500

    Added data arrays to pull from

---
+++ src/data.js
@@ -0,0 +1,47 @@
+export const projects = [
+    {
+      title: "Art Gallery",
+      subtitle: "E-Commerce ",
+      description:
+        "A group project where we implimented HandlebarsJS and Stripe",
+      image: "/Assets/artsell.PNG",
+      link: "https://artsell.herokuapp.com/",
+    },
+    {
+      title: "Weather",
+      subtitle: "Api Calls",
+      description:
+        "This project was a good introduction to using API's to retrieve data and display it in a digestable way",
+      image: "/Assets/Weather_app_demo.png",
+      link: "https://tylaw93.github.io/Weather_App/",
+    },
+    {
+      title: "Museum",
+      subtitle: "Map Data",
+      description:
+        "This group project was handled by myself and one other developer. The api for this project allowed for us to introduce a lot of dynamic features",
+      image: "/Assets/museum_hunter_demo.png",
+      link: "https://tylaw93.github.io/Travel_project/",
+    },
+    {
+      title: "Password Generator",
+      subtitle: "User Input",
+      description: "This project allowed me to experiment with ways of taking in user input in order to generate a password.",
+      image: "/Assets/Generate_Password_demo.png",
+      link: "https://tylaw93.github.io/wustl-Password_Generator/",
+    },
+  ];
+
+export const skills = [
+     "Javascript",
+     "HTML",
+     "CSS",
+     "React",
+     "SQL",
+     "GraphQL",
+     "Node",
+     "JWS",
+     "MERN",
+     "Python"
+    
+]
